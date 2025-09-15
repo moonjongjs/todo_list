71766610c8a6b4bff4f821cfeebb63793cc408a8
moonjongjs <moonseonjong@naver.com>
2025-09-15 14:50:14 +0900

    NextJS Vercel Deploy

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
                 // 클라이언트가 /todo_list/... 로 호출하면
                 source: '/todo_list/:path*',
                 // Next 서버가 외부 원본으로 프록시
-                destination: 'https://moonjong.dothome.co.kr/todo_list/:path*'
+                destination: 'http://moonjong.dothome.co.kr/todo_list/:path*'
             }
         ]
     }
